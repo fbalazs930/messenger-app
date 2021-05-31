fb0ad725f7adedc9a67b52ad3c3a7b811c985b2e
fbalazs930 <fbalazs930@gmail.com>
2021-05-31 19:59:34 +0200

    up

--- src/Message.js
+++ src/Message.js
@@ -4,7 +4,8 @@ export default function Message({message, username}) {
     const isUser = username === message.username;
     return (
         <div>
-            <p className={`message ${isUser ? 'user' : 'not-user'}`}>{message.username}: {message.message}</p>
+            <p className={`message ${isUser ? 'user' : 'not-user'}`}>{message.username}: {message.message}
+            </p>
         </div>
     )
 }
