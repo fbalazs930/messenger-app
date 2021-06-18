84df85df41528078c5c75130a0ecfc663803f623
fbalazs930 <fbalazs930@gmail.com>
2021-06-18 08:06:50 +0200

    Expired Firebase -> new FB

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyA-Yxmc3Wrh23RBVFnMS7J6jQnBA0FP5Wk",
-    authDomain: "messenger-app-fb.firebaseapp.com",
-    projectId: "messenger-app-fb",
-    storageBucket: "messenger-app-fb.appspot.com",
-    messagingSenderId: "301382312978",
-    appId: "1:301382312978:web:b1cfef02462469c659e4cb",
-    measurementId: "G-YLKV4BMYRV"
+    apiKey: "AIzaSyBojXxEz5lOXbzTr7ym2tICSXtPhsBIHKA",
+    authDomain: "messenger-app-fb-dbe97.firebaseapp.com",
+    projectId: "messenger-app-fb-dbe97",
+    storageBucket: "messenger-app-fb-dbe97.appspot.com",
+    messagingSenderId: "90681988714",
+    appId: "1:90681988714:web:2311c223181570f7ee9c13",
+    measurementId: "G-1YVZN8QTLE"
 });
 
 const db = firebaseApp.firestore();
