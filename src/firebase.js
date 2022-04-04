3d80ecf4f86c48762ced92e1b8b5b294b19845c2
fbalazs930 <fbalazs930@gmail.com>
2022-04-04 12:54:16 +0200

    new db

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBm1S8MhqncQaQ_eosumWYa7x-j_Wh30_M",
-    authDomain: "messenger-app-23b86.firebaseapp.com",
-    projectId: "messenger-app-23b86",
-    storageBucket: "messenger-app-23b86.appspot.com",
-    messagingSenderId: "832687519463",
-    appId: "1:832687519463:web:dabbd927e3637d24a6d442",
-    measurementId: "G-MVZRX07CD6"
+    apiKey: "AIzaSyCWjfskd4M5MCxETlfwNrDlW0Zb0pT8iVo",
+    authDomain: "messenger-app-45ec2.firebaseapp.com",
+    projectId: "messenger-app-45ec2",
+    storageBucket: "messenger-app-45ec2.appspot.com",
+    messagingSenderId: "808187174773",
+    appId: "1:808187174773:web:68a71d69d090d57b5c7d3c",
+    measurementId: "G-M4VGS5GR59"
 });
 
 const db = firebaseApp.firestore();
